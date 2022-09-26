5d82ba833b6afcca9f53a9d030160a4c4c539e9a
EnmaJim <p.jimenez@sisalril.gob.do>
2022-09-26 09:38:53 -0400

    add required field attrib

--- DTO/UsuarioDTO.cs
+++ DTO/UsuarioDTO.cs
@@ -1,8 +1,13 @@
-﻿namespace DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DTO
 {
     public class UsuarioDTO
     {
+        [Required]
         public string UserName { get; set; }
+
+        [Required]
         public string Password { get; set; }
     }
 }

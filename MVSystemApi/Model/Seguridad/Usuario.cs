6cdf83a82dac57ea21f440c0f1760c19ab1d5ff1
xavierms <xavierams08@gmail.com>
2023-03-01 19:07:19 -0400

    Seguridad updated

--- MVSystemApi/Model/Seguridad/Usuario.cs
+++ MVSystemApi/Model/Seguridad/Usuario.cs
@@ -7,7 +7,10 @@
         public string Descripcion { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
-        public int Empresa { get; set; }
+        public int Id_Empresa { get; set; }
+        public string Empresa { get; set; }
+        public int Id_Sucursal { get; set; }
+        public string Sucursal { get; set; }
         public int Estado { get; set; }
         public DateTime FechaRegistro { get; set; }
         public DateTime FechaModificado { get; set; }
